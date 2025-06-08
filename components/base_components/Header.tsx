59c060a580cb069ad480e339936c29881305bbde
shrirajnaik04 <nykshriraj4nov@gmail.com>
2025-06-08 14:25:36 +0530

    refactor: updated header logo as per clients request.

--- components/base_components/Header.tsx
+++ components/base_components/Header.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
-import { Truck } from "lucide-react";
+import Link from "next/link"
 import MobileNav from "./MobileNav"
-
 
 const navLinks = [
   { label: "Home", href: "/" },
@@ -14,17 +12,21 @@ const navLinks = [
 export default function Header() {
   return (
     <header
-      className="bg-white flex justify-center px-4 lg:px-16 items-center w-full shadow-md sticky top-0 z-50  font-sans"
+      className="bg-white flex justify-center px-4 lg:px-16 items-center w-full shadow-md sticky top-0 z-50 font-sans"
       role="banner"
     >
-      <div className="container ">
+      <div className="container">
         <div className="flex justify-between items-center h-16">
           {/* Logo and Brand */}
           <div
             className="flex items-center space-x-2"
             aria-label="JK Cargocare Logo"
           >
-            <Truck className="h-8 w-8 text-blue-600" aria-hidden="true" />
+            <img
+              src="/assets/images/jkc-logo.png"
+              alt="JK Cargocare"
+              className="h-11 w-auto"
+            />
             <span className="text-xl font-bold text-gray-800">
               JK Cargocare
             </span>
@@ -37,9 +39,8 @@ export default function Header() {
             aria-label="Primary navigation"
           >
             {navLinks.map(({ label, href }) => (
-              <div key={href} className=" h-full">
+              <div key={href} className="h-full">
                 <Link
-                  key={href}
                   href={href}
                   className="relative text-gray-700 hover:text-blue-600 font-belleza after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all after:duration-300"
                 >
